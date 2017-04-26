9cd95c19113e1abd7598643d86d86d7258c63d95
abdullahawara <abdulla@abdullas-MacBook-Pro.local>
2017-04-26 15:15:12 +0200

    added sleep time to avoid failures

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -43,9 +43,10 @@ public class WebTest extends AbstractTest {
 	}
 
 	@Test
-	public void openWebpageAndTakeScreenshot() {
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void openWebpageAndTakeScreenshot() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
+		Thread.sleep(10000);
 		String url = "http://www.amazon.com";
 		driver.get(url);
 		driver.rotate(ScreenOrientation.LANDSCAPE);
