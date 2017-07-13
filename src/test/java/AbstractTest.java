10c385623b2d6b86dfce36539acbd97829b24ccc
crummy <crummynz@gmail.com>
2017-07-13 15:05:29 +0200

    Print message before allocating driver

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -51,6 +51,7 @@ public abstract class AbstractTest {
 		System.out.println("TestUUID: " + testUUID);
 		capabilities.setCapability("testobject_testuuid", testUUID);
 
+		System.out.println("Allocating driver with capabilities:\n" + capabilities)
 		driver = new IOSDriver(endpoint, capabilities);
 
 		System.out.println(driver.getCapabilities().getCapability("testobject_test_report_url"));
